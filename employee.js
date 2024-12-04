ed1e613d7b18acf35aca57d25ab30a5eb06723d2
gourang001 <gourangchauhan001@gmail.com>
2024-12-04 13:51:32 +0530

    implement task 3

--- employee.js
+++ employee.js
@@ -1,38 +1,38 @@
-// Ability to Check Employee is present or Absent 
+// Refactor the Code to write a function to get work hours
 const PRESENT = 1;
 let attendance = Math.floor(Math.random() * 2); 
 
-if(attendance == PRESENT){
+if (attendance === PRESENT) {
   console.log("Employee is Present");
-const wage = 20;
-const partTime = 4;
-const fullTime = 8;
-const noTime = 0;
 
-let work = Math.floor(Math.random() * 3); 
+  const wage = 20;
+  const partTime = 4;
+  const fullTime = 8;
+  const noTime = 0;
 
-let hoursWorked;
+  let work = Math.floor(Math.random() * 3); 
 
-if (work === 0) {
-  hoursWorked = noTime;
-  console.log("Employee did not work today.");
+  function workdone(work) {
+    if(work === 0){
+      console.log("Employee did not work today.");
+      return noTime;
+    } 
+    else if(work === 1){
+      console.log("Employee worked Part-Time today.");
+      return partTime;
+    } 
+    else if(work === 2){
+      console.log("Employee worked Full-Time today.");
+      return fullTime;
+    }
+  }
+
+  let hoursWorked = workdone(work); 
+  let dailyWage = hoursWorked * wage;
+
+  console.log(`Hours Worked: ${hoursWorked}`);
+  console.log(`Daily Wage: $${dailyWage}`);
 } 
-else if (work === 1) {
-  hoursWorked = partTime;
-  console.log("Employee worked Part-Time today.");
-} 
-else if (work === 2) {
-  hoursWorked = fullTime;
-  console.log("Employee worked Full-Time today.");
-}
-
-
-let dailyWage = hoursWorked * wage;
-
-console.log(`Hours Worked: ${hoursWorked}`);
-console.log(`Daily Wage: ${dailyWage}`);
-
-}
 else{
   console.log("Employee is Absent");
 }
