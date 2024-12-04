fe14659ed4368d2fda4986ff1e1d0857f4e178f7
gourang001 <gourangchauhan001@gmail.com>
2024-12-04 14:49:15 +0530

    implement task 5

--- employee.js
+++ employee.js
@@ -3,7 +3,7 @@ const partTime = 4;
 const fullTime = 8;
 const noTime = 0;
 const workingDays = 20;
-
+const maxWorkingHour = 160;
 
 function getWorkHours(workType) {
   if (workType === 0) {
@@ -12,29 +12,39 @@ function getWorkHours(workType) {
     return partTime; 
   } else if (workType === 2) {
     return fullTime; 
-  } 
+  }
 }
 
+function calculateDailyWage(hours) {
+  return hours * wage;
+}
 
 function calculateMonthlyWages() {
-  let totalWages = 0;
-  let totalHours = 0;
+  let totalWorkingHours = 0;
+  let totalWorkingDays = 0;
 
-  for (let day = 1; day <= workingDays; day++) {
+  while (totalWorkingHours < maxWorkingHour && totalWorkingDays < workingDays) {
+    totalWorkingDays++;
     const workType = Math.floor(Math.random() * 3); 
     const dailyHours = getWorkHours(workType);
-    const dailyWage = dailyHours * wage;
 
-    totalHours += dailyHours;
-    totalWages += dailyWage;
+    totalWorkingHours += dailyHours;
 
-    console.log(`Day ${day}: Worked ${dailyHours} hours, Earned $${dailyWage}`);
+    if (totalWorkingHours > maxWorkingHour) {
+      totalWorkingHours = maxWorkingHour;
+    }
+
+    const dailyWage = calculateDailyWage(dailyHours);
+
+    console.log(`Day ${totalWorkingDays}: Worked ${dailyHours} hours, Earned $${dailyWage}`);
   }
 
-  console.log(`\nTotal Hours Worked in the Month: ${totalHours}`);
-  console.log(`Total Wages for the Month: $${totalWages}`);
-}
+  const totalWage = calculateDailyWage(totalWorkingHours);
 
+  console.log(`\nTotal Working Days: ${totalWorkingDays}`);
+  console.log(`Total Working Hours: ${totalWorkingHours}`);
+  console.log(`Total Wage: $${totalWage}`);
+}
 
 const PRESENT = 1; 
 let attendance = Math.floor(Math.random() * 2); 
