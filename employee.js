cea23487f04e4af0948014e9fd804a1a7e71265e
gourang001 <gourangchauhan001@gmail.com>
2024-12-04 13:21:01 +0530

    implement task 1

---
+++ employee.js
@@ -0,0 +1,11 @@
+// Ability to Check Employee is present or Absent 
+const PRESENT = 1;
+let attendance = Math.floor(Math.random() * 2); 
+
+if(attendance == PRESENT){
+  console.log("Employee is Present");
+  
+}
+else{
+  console.log("Employee is Absent");
+}
